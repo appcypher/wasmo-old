9acc370f0035836d406a18aa85e570abe1fe1d56
Steve Akinyemi <appcypher@outlook.com>
2019-06-22 11:27:45 +0100

    Add test examples

--- examples/parser.rs
+++ examples/parser.rs
@@ -6,8 +6,12 @@ use wasmo_utils::{debug, file::wat2wasm, verbose};
 
 fn main() {
     // Convert wat file to wasm bytes.
-    // let wasm_bytes = wat2wasm(project_path("examples/wat/valid_export_section.wat").as_str());
-    let wasm_bytes = wat2wasm(project_path("examples/wat/valid_add_operations.wat").as_str());
+    // let wasm_bytes = wat2wasm(project_path("examples/wat/valid_export.wat").as_str());
+    // let wasm_bytes = wat2wasm(project_path("examples/wat/valid_add.wat").as_str());
+    // let wasm_bytes = wat2wasm(project_path("examples/wat/valid_block.wat").as_str());
+    // let wasm_bytes = wat2wasm(project_path("examples/wat/valid_more_on_stack.wat").as_str());
+    // let wasm_bytes = wat2wasm(project_path("examples/wat/valid_multiple_block.wat").as_str());
+    let wasm_bytes = wat2wasm(project_path("examples/wat/valid_empty_block.wat").as_str());
 
     // Parse wasm bytes
     let wasm_module = Parser::new(&wasm_bytes).module();
