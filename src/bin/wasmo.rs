44214ce94b2d7c801428d8d05a1ee49c2f74a84c
Steve Akinyemi <appcypher@outlook.com>
2019-06-23 17:36:42 +0100

    Clean entry file

--- src/bin/wasmo.rs
+++ src/bin/wasmo.rs
@@ -5,7 +5,7 @@ use wasmo_utils::verbose;
 // Use clap for argument parsing.
 
 fn main() {
-    println!("\n=== [ wasmo ] ===\n");
+    println!("\n === [ wasmo ] ===\n");
 
     println!("|                   |");
     println!("|    STAY TUNED!    |");
@@ -14,15 +14,7 @@ fn main() {
     // Get all arguments.
     let args: Vec<String> = env::args().collect();
 
-    // Get path to running executable.
-    let path_to_exe = env::current_exe().unwrap();
-
-    // Get executable path as string.
-    let path_to_exe = path_to_exe.into_os_string().into_string().unwrap();
-
-    verbose!("executable path = {:?}\n", path_to_exe);
-
     verbose!("args = {:#?}\n", args);
 
-    println!("\n=== [ wasmo ] ===\n");
+    println!("\n === [ wasmo ] ===\n");
 }
